0d77aa3d28fbaa000ec373fd2b838ef3470beaac
Fonsi17 <kenneth.fex@gmail.com>
2024-05-23 22:05:29 -0600

    tarea

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+
+const server = express();
+server.use(express.static('public'));
+
+function hello (req, res){
+    res.send("hello world")
+    server.get("/", hello)
+}
+
+server.get("/", hello)
+server.listen(3000, ()=>{
+    console.log("server running")
+})
---
+++ public/app.js
@@ -0,0 +1,5 @@
+const helloButton = document.getElementById('helloButton');
+
+helloButton.addEventListener('click', () => {
+    alert('Hello World!');
+});
